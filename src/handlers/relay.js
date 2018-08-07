f759fd70d801d9416aef4ada3b4b74330affa5f3
Veronica Zheng <zhengzhixing@gmail.com>
2018-08-06 19:32:54 -0700

    remove verify auth and verify MetaTx signature

--- src/handlers/relay.js
+++ src/handlers/relay.js
@@ -22,19 +22,19 @@ class RelayHandler {
   constructor(authMgr, ethereumMgr, metaTxMgr) {
     this.authMgr = authMgr;
     this.ethereumMgr = ethereumMgr;
-    this.metaTxMgr = metaTxMgr;
+    //this.metaTxMgr = metaTxMgr;
   }
 
   async handle(event, context, cb) {
-    let authToken;
-    try {
-      authToken = await this.authMgr.verifyNisaba(event);
-    } catch (err) {
-      console.log("Error on this.authMgr.verifyNisaba");
-      console.log(err);
-      cb({ code: 401, message: err.message });
-      return;
-    }
+    // let authToken;
+    // try {
+    //   authToken = await this.authMgr.verifyNisaba(event);
+    // } catch (err) {
+    //   console.log("Error on this.authMgr.verifyNisaba");
+    //   console.log(err);
+    //   cb({ code: 401, message: err.message });
+    //   return;
+    // }
 
     let body;
 
@@ -62,36 +62,36 @@ class RelayHandler {
     }
 
     // support hex strings starting with 0x
-    if (body.metaSignedTx.startsWith("0x")) {
-      body.metaSignedTx = body.metaSignedTx.slice(2);
-    }
+    // if (body.metaSignedTx.startsWith("0x")) {
+    //   body.metaSignedTx = body.metaSignedTx.slice(2);
+    // }
+    //
+    // //Check if metaTx signature is valid
+    // if (!(await this.metaTxMgr.isMetaSignatureValid(body))) {
+    //   cb({ code: 403, message: "MetaTx signature invalid" });
+    //   return;
+    // }
+    //
+    // let decodedMetaTx;
+    // try {
+    //   decodedMetaTx = await this.metaTxMgr.decodeMetaTx(body.metaSignedTx);
+    // } catch (err) {
+    //   console.log("Error on this.metaTxMgr.decodedMetaTx");
+    //   console.log(err);
+    //   cb({ code: 403, message: err.message });
+    //   return;
+    // }
 
-    //Check if metaTx signature is valid
-    if (!(await this.metaTxMgr.isMetaSignatureValid(body))) {
-      cb({ code: 403, message: "MetaTx signature invalid" });
-      return;
-    }
-
-    let decodedMetaTx;
-    try {
-      decodedMetaTx = await this.metaTxMgr.decodeMetaTx(body.metaSignedTx);
-    } catch (err) {
-      console.log("Error on this.metaTxMgr.decodedMetaTx");
-      console.log(err);
-      cb({ code: 403, message: err.message });
-      return;
-    }
-
-    //Verify auth.sub and decodedMetaTx.claimedAddress
-    if (authToken.sub !== decodedMetaTx.claimedAddress) {
-      console.log("authToken.sub !== decodedMetaTx.claimedAddress");
-      cb({
-        code: 403,
-        message:
-          "Auth token mismatch. Does not match `claimedAddress` field in metatx"
-      });
-      return;
-    }
+    // //Verify auth.sub and decodedMetaTx.claimedAddress
+    // if (authToken.sub !== decodedMetaTx.claimedAddress) {
+    //   console.log("authToken.sub !== decodedMetaTx.claimedAddress");
+    //   cb({
+    //     code: 403,
+    //     message:
+    //       "Auth token mismatch. Does not match `claimedAddress` field in metatx"
+    //   });
+    //   return;
+    // }
 
     //gets signed transaction
     let signedRawTx;
@@ -107,7 +107,7 @@ class RelayHandler {
       return;
     }
 
-    //sets transaction hash from created and sent signed transaction 
+    //sets transaction hash from created and sent signed transaction
     try {
       const txHash = await this.ethereumMgr.sendRawTransaction(
         signedRawTx,
